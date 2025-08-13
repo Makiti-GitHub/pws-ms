757b0988aa23333b4c617cc48cd114244dd64a23
Fenyep <0Uiimi1D5/-yYkGt>
2025-08-13 10:07:50 +0100

    feat: Add legal notice page with structured content and translations

--- src/app/legal_notice/LegalNotice.tsx
+++ src/app/legal_notice/LegalNotice.tsx
@@ -4,18 +4,204 @@ import { useTranslation } from 'react-i18next'
 const LegalNotice = () => {
 	const { t } = useTranslation()
 	return (
-		<main className="flex flex-col gap-4 sm:gap-8 px-4 sm:px-8 md:px-[80px] lg:px-[120px] pt-[150px] relative">
-			<h1 className="font-seravek_bold text-3xl">{t('pages.termsAndConditions.title')}</h1>
+		<main className="flex flex-col gap-4 sm:gap-8 px-4 sm:px-8 md:px-[80px] lg:px-[120px] pt-[150px] pb-14 relative">
+			<h1 className="font-seravek_bold text-3xl">{t('pages.legalNotice.title')}</h1>
 
 			{/** Section 1 */}
 			<div className="space-y-4">
 				<h2 className="font-seravek_bold text-2xl">
-					1. {t('pages.termsAndConditions.sections.introduction.title')}
+					1. {t('pages.legalNotice.sections.websitePublisher.title')}
 				</h2>
 
 				<div className="space-y-2 text-lg">
-					<TransParagraph transKey="pages.termsAndConditions.sections.introduction.p1" />
-					<TransParagraph transKey="pages.termsAndConditions.sections.introduction.p2" />
+					<TransParagraph transKey="pages.legalNotice.sections.websitePublisher.p1" />
+				</div>
+			</div>
+
+			{/** Section 2 */}
+			<div className="space-y-4">
+				<h2 className="font-seravek_bold text-2xl">
+					2. {t('pages.legalNotice.sections.headOffice.title')}
+				</h2>
+
+				<div className="space-y-2 text-lg">
+					<TransParagraph transKey="pages.legalNotice.sections.headOffice.p1" />
+					<TransParagraph
+						transKey="pages.legalNotice.sections.headOffice.p2"
+						phones={['+237 680 095 453']}
+					/>
+					<TransParagraph
+						transKey="pages.legalNotice.sections.headOffice.p3"
+						emails={['contact@makiti-group.com']}
+					/>
+					<TransParagraph transKey="pages.legalNotice.sections.headOffice.p4" />
+				</div>
+			</div>
+
+			{/** Section 3 */}
+			<div className="space-y-4">
+				<h2 className="font-seravek_bold text-2xl">
+					3. {t('pages.legalNotice.sections.hosting.title')}
+				</h2>
+
+				<div className="space-y-2 text-lg">
+					<TransParagraph transKey="pages.legalNotice.sections.hosting.p1" />
+					<TransParagraph transKey="pages.legalNotice.sections.hosting.p2" />
+					<TransParagraph
+						transKey="pages.legalNotice.sections.hosting.p3"
+						phones={['+49 89 3564717-70']}
+					/>
+					<TransParagraph
+						transKey="pages.legalNotice.sections.hosting.p4"
+						emails={['support@contabo.de']}
+					/>
+				</div>
+			</div>
+
+			{/** Section 4 */}
+			<div className="space-y-4">
+				<h2 className="font-seravek_bold text-2xl">
+					4. {t('pages.legalNotice.sections.websitePurpose.title')}
+				</h2>
+
+				<div className="space-y-2 text-lg">
+					<TransParagraph
+						transKey="pages.legalNotice.sections.websitePurpose.p1"
+						links={['https://www.makiti-group.com']}
+					/>
+				</div>
+			</div>
+
+			{/** Section 5 */}
+			<div className="space-y-4">
+				<h2 className="font-seravek_bold text-2xl">
+					5. {t('pages.legalNotice.sections.intellectualProperty.title')}
+				</h2>
+
+				<div className="space-y-2 text-lg">
+					<TransParagraph transKey="pages.legalNotice.sections.intellectualProperty.p1" />
+					<TransParagraph transKey="pages.legalNotice.sections.intellectualProperty.p2" />
+				</div>
+			</div>
+
+			{/** Section 6 */}
+			<div className="space-y-4">
+				<h2 className="font-seravek_bold text-2xl">
+					6. {t('pages.legalNotice.sections.dataProtection.title')}
+				</h2>
+
+				<div className="space-y-2 text-lg">
+					<TransParagraph transKey="pages.legalNotice.sections.dataProtection.p1" />
+				</div>
+			</div>
+
+			{/** Section 7 */}
+			<div className="space-y-4">
+				<h2 className="font-seravek_bold text-2xl">
+					7. {t('pages.legalNotice.sections.collectedData.title')}
+				</h2>
+
+				<div className="space-y-2 text-lg">
+					<TransParagraph transKey="pages.legalNotice.sections.collectedData.p1" />
+				</div>
+			</div>
+
+			{/** Section 8 */}
+			<div className="space-y-4">
+				<h2 className="font-seravek_bold text-2xl">
+					8. {t('pages.legalNotice.sections.processingPurposes.title')}
+				</h2>
+
+				<div className="space-y-2 text-lg">
+					<TransParagraph transKey="pages.legalNotice.sections.processingPurposes.p1" />
+				</div>
+			</div>
+
+			{/** Section 9 */}
+			<div className="space-y-4">
+				<h2 className="font-seravek_bold text-2xl">
+					9. {t('pages.legalNotice.sections.retentionPeriod.title')}
+				</h2>
+
+				<div className="space-y-2 text-lg">
+					<TransParagraph transKey="pages.legalNotice.sections.retentionPeriod.p1" />
+				</div>
+			</div>
+
+			{/** Section 10 */}
+			<div className="space-y-4">
+				<h2 className="font-seravek_bold text-2xl">
+					10. {t('pages.legalNotice.sections.userRights.title')}
+				</h2>
+
+				<div className="space-y-2 text-lg">
+					<TransParagraph transKey="pages.legalNotice.sections.userRights.p1" />
+					<TransParagraph transKey="pages.legalNotice.sections.userRights.p2" />
+				</div>
+			</div>
+
+			{/** Section 11 */}
+			<div className="space-y-4">
+				<h2 className="font-seravek_bold text-2xl">
+					11. {t('pages.legalNotice.sections.dataTransfer.title')}
+				</h2>
+
+				<div className="space-y-2 text-lg">
+					<TransParagraph transKey="pages.legalNotice.sections.dataTransfer.p1" />
+				</div>
+			</div>
+
+			{/** Section 12 */}
+			<div className="space-y-4">
+				<h2 className="font-seravek_bold text-2xl">
+					12. {t('pages.legalNotice.sections.cookies.title')}
+				</h2>
+
+				<div className="space-y-2 text-lg">
+					<TransParagraph transKey="pages.legalNotice.sections.cookies.p1" />
+					<TransParagraph transKey="pages.legalNotice.sections.cookies.p2" />
+				</div>
+			</div>
+
+			{/** Section 13 */}
+			<div className="space-y-4">
+				<h2 className="font-seravek_bold text-2xl">
+					13. {t('pages.legalNotice.sections.liability.title')}
+				</h2>
+
+				<div className="space-y-2 text-lg">
+					<TransParagraph transKey="pages.legalNotice.sections.liability.p1" />
+				</div>
+			</div>
+
+			{/** Section 14 */}
+			<div className="space-y-4">
+				<h2 className="font-seravek_bold text-2xl">
+					14. {t('pages.legalNotice.sections.applicableLawAndJuridiction.title')}
+				</h2>
+
+				<div className="space-y-2 text-lg">
+					<TransParagraph transKey="pages.legalNotice.sections.applicableLawAndJuridiction.p1" />
+				</div>
+			</div>
+
+			{/** Section 15 */}
+			<div className="space-y-4">
+				<h2 className="font-seravek_bold text-2xl">
+					15. {t('pages.legalNotice.sections.contact.title')}
+				</h2>
+
+				<div className="space-y-2 text-lg">
+					<TransParagraph transKey="pages.legalNotice.sections.contact.p1" />
+					<TransParagraph
+						transKey="pages.legalNotice.sections.contact.p2"
+						emails={['contact@makiti.de']}
+					/>
+					<TransParagraph transKey="pages.legalNotice.sections.contact.p3" />
+					<TransParagraph
+						transKey="pages.legalNotice.sections.contact.p4"
+						phones={['+237 680 095 453']}
+					/>
 				</div>
 			</div>
 		</main>
--- src/i18n/i18n.ts
+++ src/i18n/i18n.ts
@@ -1,4 +1,3 @@
-// src/i18n.ts
 import i18n from 'i18next'
 import { initReactI18next } from 'react-i18next'
 import LanguageDetector from 'i18next-browser-languagedetector'
@@ -6,7 +5,6 @@ import Backend from 'i18next-http-backend'
 import en from '../locales/en/translation.json'
 import de from '../locales/de/translation.json'
 import fr from '../locales/fr/translation.json'
-// import fr from '../locales/de/translation.json';
 
 const resources = {
 	en: {
