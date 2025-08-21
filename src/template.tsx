31d407f8a9abd677ba6a686f3f191edf9ec012ff
Fenyep <0Uiimi1D5/-yYkGt>
2025-08-21 18:17:06 +0100

    added favicons

--- src/template.tsx
+++ src/template.tsx
@@ -5,7 +5,12 @@ export default function Template({ Head, Body, Script }: TemplateProps) {
 		<html lang="en">
 			<Head>
 				<meta charSet="UTF-8" />
-				<link rel="icon" type="image/x-icon" href="/makiti_sarl.ico" />
+
+				{/* <!-- Standard Favicon --> */}
+				<link rel="icon" type="image/png" href="/favicon/favicon-16x16.png" sizes="16x16" />
+				<link rel="icon" type="image/png" href="/favicon/favicon-32x32.png" sizes="32x32" />
+				<link rel="icon" type="image/svg+xml" href="/favicon/favicon2.svg" />
+				{/* <link rel="icon" type="image/x-icon" href="/makiti_sarl.ico" /> */}
 				<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 			</Head>
 
